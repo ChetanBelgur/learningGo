79d1107233ab448377a72e37639e49ab587c11ae
Chetan Belgur <chetan.belgur@gmail.com>
2022-06-04 20:24:14 +0530

    Added Comments to code

--- displayInColumns.go
+++ displayInColumns.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+
+	//Loop Index is defined and used diredtly in the loop
 	for loopIndex := 0; loopIndex < 10; loopIndex++ {
 		if loopIndex%2 == 0 {
 			fmt.Print(loopIndex)
