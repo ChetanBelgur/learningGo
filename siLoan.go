849887fffd57f646a6d0367f7bdcfa2a8b1935b6
Chetan Belgur <chetan.belgur@gmail.com>
2022-06-04 20:00:33 +0530

    Added comments to code

--- siLoan.go
+++ siLoan.go
@@ -3,11 +3,14 @@ package main
 import "fmt"
 
 func main() {
+
+	//Declare Variables to compute
 	var simpleInterest, principal, annualRateOfInterest float32
 	var loanTenure int
 
 	var amount float32
 
+	//Ask User Inputs
 	fmt.Print("Enter Borrowed Loan Amount: ")
 	fmt.Scan(&principal)
 
@@ -17,9 +20,11 @@ func main() {
 	fmt.Print("Loan Tenure: ")
 	fmt.Scan(&loanTenure)
 
+	//Compute simple Interest and final amount to pay
 	simpleInterest = (principal * annualRateOfInterest * float32(loanTenure)) / 100
 	amount = simpleInterest + principal
 
+	//Print computed values
 	fmt.Println("Borrowed Amount: ", principal)
 	fmt.Println("Simple Interest:", simpleInterest)
 	fmt.Println("Total Amount: ", amount)
